ec590c7a9800c1ee7d50607cdd148c2685210cfc
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2024-06-01 16:36:50 +0700

    feat: workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  base: "/bakti-nusantara/"
-})
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [svelte()],
+    base: process.env.VITE_NODE_ENV == "testing" ? "/bakti-nusantara/" : ""
+  })
+}
